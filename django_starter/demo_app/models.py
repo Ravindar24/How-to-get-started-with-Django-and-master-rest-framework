2d3346466f91598ad85ba02ec9885e3bb44e4654
Ravindar Sharma <ravindar24sharma@gmail.com>
2019-08-10 12:05:07 +0530

    added frontend

--- django_starter/demo_app/models.py
+++ django_starter/demo_app/models.py
@@ -21,33 +21,39 @@ class CustomerInformation(models.Model):
     first_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
     gender = models.CharField(max_length = 10)
-    address = models.CharField(max_length = 200)
     email = models.EmailField(max_length = 50)
-    contact_number = models.IntegerField()
-    pin_code =  models.IntegerField()
 
 
     def __init__(self,\
                  first_name,\
                  last_name,\
                  gender,\
-                 address,\
-                 email,\
-                 contact_number,\
-                 pin_code):
+                 email):
 
         self.first_name = first_name
         self.last_name = last_name
         self.gender = gender
-        self.address = address
         self.email = email
+
+
+
+class CustomerAddress(models.Model):
+
+    address = models.CharField(max_length = 200)
+    contact_number = models.IntegerField()
+    pin_code =  models.IntegerField()
+    customer = models.ForeignKey(CustomerInformation , on_delete = models.CASCADE)
+
+    def __init(self,\
+            address,\
+            contact_number,\
+            pin_code,\
+            customerInfo):
+            
+        self.address = address
         self.contact_number = contact_number
         self.pin_code = pin_code
-
-
-
-
-
+        self.customer = customerInfo
 
 
 
