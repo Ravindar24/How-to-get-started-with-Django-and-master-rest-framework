920ac8ccd10b7f7e3eb53fe6621a11094cd26c2a
Ravindar Sharma <ravindar24sharma@gmail.com>
2019-08-10 10:10:19 +0530

    adding demo_app

--- django_starter/demo_app/models.py
+++ django_starter/demo_app/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+@Author Ravindar Sharma
+
+Description About models.py
+
+A models.py is just a simple python file which basically
+is created for each django application for ORM.
+
+ORM is a paradigm to proceess and deal with objects and database with CRUD opertions.
+
+All the fields defined in the class will act as columns of the table.
+class name will be used as table name
+
+'''
+class CustomerInformation(models.Model):
+    
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    gender = models.CharField(max_length = 10)
+    address = models.CharField(max_length = 200)
+    email = models.EmailField(max_length = 50)
+    contact_number = models.IntegerField()
+    pin_code =  models.IntegerField()
+
+
+    def __init__(self,\
+                 first_name,\
+                 last_name,\
+                 gender,\
+                 address,\
+                 email,\
+                 contact_number,\
+                 pin_code):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gender = gender
+        self.address = address
+        self.email = email
+        self.contact_number = contact_number
+        self.pin_code = pin_code
+
+
+
+
+
+
+
+
+
+
+
+
--- django_starter/demo_app/views.py
+++ django_starter/demo_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class CustomerView(APIView):
+
+    #method to be called when a GET request is made
+    def get(self,request):
+        get_message = "This is a message from get request"
+        return Response({"message" : get_message})
+
+    #method to be called when a POST request is made
+    def post(self,request):
+        post_message = "This is a message from post request"
+        return Response({"message" : post_message})
+
